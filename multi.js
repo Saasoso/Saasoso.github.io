0945199a72e9215128c1ebbc3ca005325849750e
Saad <Saasoso@users.noreply.github.com>
2022-12-26 12:28:38 +0100

    New small website

---
+++ multi.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.img');
+var text1 = document.querySelector('.text1');
+var text2 = document.querySelector('.text2');
+var text3 = document.querySelector('.text3');
+
+function phones(phone){
+    img.src = phone;
+
+}
